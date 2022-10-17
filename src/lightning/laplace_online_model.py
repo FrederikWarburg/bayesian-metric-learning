018eaf035ae2cdb0a17740f879bf5f0538aa518c
Frederik Warburg <frwa@comp-pcfrwa.compute.dtu.dk>
2022-10-17 11:23:56 +0200

    updated to reflect new functionalities in stochman

--- src/lightning/laplace_online_model.py
+++ src/lightning/laplace_online_model.py
@@ -6,28 +6,11 @@ import sys
 
 sys.path.append("../stochman")
 from stochman import nnj
-from stochman.laplace import HessianCalculator
-from models.laplace_utils import DiagLaplace
+from stochman.hessian import HessianCalculator
+from stochman.laplace import DiagLaplace
+from stochman.utils import convert_to_stochman
 from torch.nn.utils import parameters_to_vector, vector_to_parameters
 from tqdm import tqdm
-
-def convert_to_stochman(sequential):
-
-    model = []
-    for layer in sequential:
-
-        if layer.__class__.__name__ == "Linear":
-            stochman_layer = getattr(nnj, layer.__class__.__name__)(layer.in_features, layer.out_features)
-        elif layer.__class__.__name__ == "Conv2d":
-            stochman_layer = getattr(nnj, layer.__class__.__name__)(layer.in_channel, layer.out_channel, layer.kernel)
-        else:
-            stochman_layer = getattr(nnj, layer.__class__.__name__)()
-
-        model.append(stochman_layer)
-
-    model = nnj.Sequential(model, add_hooks=True)
-
-    return model
 
 
 class LaplaceOnlineModel(Base):
