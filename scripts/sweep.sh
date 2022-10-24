659e09072191e12a0c7befa846f8d0208e13f2ff
Frederik Rahbaek Warburg <frewar1905@gmail.com>
2022-10-24 17:27:16 +0200

    sweep script

--- scripts/sweep.sh
+++ scripts/sweep.sh
@@ -32,11 +32,4 @@ module load cuda/11.3
 module load gcc/9.2.0 
 source activate metric_learning
 
-python run.py --config ../configs/msls/deterministic.yaml
-#python run.py --config ../configs/msls/pfe.yaml
-###python run.py --config ../configs/msls/laplace_posthoc_fix.yaml
-###python run.py --config ../configs/msls/laplace_posthoc_full.yaml
-###python run.py --config ../configs/msls/laplace_posthoc_pos.yaml
-###python run.py --config ../configs/msls/laplace_online_fix.yaml
-###python run.py --config ../configs/msls/laplace_online_full.yaml
-###python run.py --config ../configs/msls/laplace_online_pos.yaml
+python miner_sweep.py
