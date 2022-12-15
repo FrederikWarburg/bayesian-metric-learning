34a6c15ad5bced334656061f545842f9fc96012e
Silas Brack <57684859+silasbrack@users.noreply.github.com>
2022-12-15 14:04:52 +0100

    Added my LFW. (#2)
    * Added working LFW.
    
    * Added submit scripts.
    
    * Added silly testing code.
    
    * Small changes to submit scripts.
    
    * Removed silly business.

--- src/lightning/laplace_online_model.py
+++ src/lightning/laplace_online_model.py
@@ -18,12 +18,8 @@ from miners.triplet_miners import TripletMarginMiner
 
 
 hessian_calculators = {
-    "contrastive_pos": ContrastiveHessianCalculator,
-    "contrastive_fix": ContrastiveHessianCalculator,
-    "contrastive_full": ContrastiveHessianCalculator,
-    "arccos_pos": ArccosHessianCalculator,
-    # "arccos_fix": ArccosHessianCalculator,
-    "arccos_full": ArccosHessianCalculator,
+    "contrastive": ContrastiveHessianCalculator,
+    "arccos": ArccosHessianCalculator,
 }
 
 
@@ -36,8 +32,7 @@ class LaplaceOnlineModel(Base):
         # transfer part of model to stochman
         self.model.linear = convert_to_stochman(self.model.linear)
 
-        loss_func = f"{args.loss}_{args.loss_approx}"
-        self.hessian_calculator = hessian_calculators[loss_func](
+        self.hessian_calculator = hessian_calculators[args.loss](
             wrt="weight", 
             shape="diagonal", 
             speed="half", 
