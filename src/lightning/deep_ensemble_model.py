6c8dde72e990a494e0dfe11e454519f71ef3c4f8
Frederik Warburg frwa@dtu.dk <frewar1905@gmail.com>
2023-01-05 10:48:35 +0000

    small bug

--- src/lightning/deep_ensemble_model.py
+++ src/lightning/deep_ensemble_model.py
@@ -47,7 +47,7 @@ class DeepEnsembleModel(Base):
         z_sigma = zs.std(dim=0)
 
         # ensure that we are on the unit sphere
-        z_mu = z_mu / torch.norm(zs, dim=-1, keepdim=True)
-
+        z_mu = z_mu / torch.norm(z_mu, dim=-1, keepdim=True)
+        
         return {"z_mu": z_mu, "z_sigma": z_sigma, "z_samples" : zs.permute(1, 0, 2)}
 
