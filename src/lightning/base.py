fixed weird behavior on msls where hessian was reset

src/lightning/base.py
@@ -188,7 +188,7 @@ class Base(pl.LightningModule):
         )
 
     def format_outputs(self, outputs):
-
+        
         z_mu = torch.cat([o["z_mu"] for o in outputs])
         if not self.place_rec:
             targets = torch.cat([o["label"] for o in outputs])

src/lightning/laplace_online_model.py
@@ -251,8 +251,7 @@ class LaplaceOnlineModel(Base):
 
         # get mean and std of posterior
         mu_q = parameters_to_vector(self.model.linear.parameters()).unsqueeze(1)
-        self.hessian = torch.relu(self.hessian)
-        sigma_q = self.laplace.posterior_scale(self.hessian)
+        sigma_q = self.laplace.posterior_scale(torch.relu(self.hessian))
 
         # draw samples
         self.nn_weight_samples = self.laplace.sample(mu_q, sigma_q, n_samples)